2fca6300ac1d7fb90a5e98365067852f3047df6a
Lance Anderson <lance.a.585@gmail.com>
2022-12-17 15:10:34 -0800

    change to new recipr app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import React, { useState } from "react";
-import Counter from './Counter'
-import CounterHooks from './CounterHooks'
-
-export const ThemeContext = React.createContext()
-
+import React from "react";
+import RecipeList from "./RecipeList";
 
 function App() {
-  console.log("Render App")
-  const [theme, setTheme] = useState('green')
   return (
-    <ThemeContext.Provider value={{ backgroundColor: theme}}>
-    Counter
-    <Counter initialCount={0}/>
-    Counter Hooks
-    <CounterHooks initialCount={0} />
-    <button onClick={() => setTheme(prevTheme => {
-      return prevTheme === 'red' ? 'blue' : 'red'
-      })}>Toggle Theme</button>
-    </ ThemeContext.Provider>
+    <RecipeList />
   )
 }
  
