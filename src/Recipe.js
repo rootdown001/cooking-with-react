238642c7e093e37ef4580ebe01769a0e9bc9c5a9
Lance Anderson <lance.a.585@gmail.com>
2022-12-17 16:09:55 -0800

    header

--- src/Recipe.js
+++ src/Recipe.js
@@ -3,8 +3,13 @@ import React from 'react'
 export default function Recipe() {
   return (
     <div>
+        // header
         <h3>Plain Chicken</h3>
-        
+        <div>
+          <button>Edit</button>
+          <button>Delete</button>
+        </div>
+
     </div>
   )
 }
